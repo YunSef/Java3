b318ed20ead92a3dfa10f4f573c197411ad5441c
yunsef <sebblonski77@gmail.com>
2023-02-27 13:43:33 +0100

    first commit

---
+++ LastCrusade.java
@@ -0,0 +1,15 @@
+class LastCrusade {
+
+	public static void main(String[] args) {
+
+		String movieName = "Indiana Jones and the Last Crusade";
+		boolean hasSeen = true;
+		int releaseDate = 1989;
+		double score = 8.2;
+
+		System.out.println(movieName);
+		System.out.println(hasSeen);
+		System.out.println(releaseDate);
+		System.out.println(score);	
+	}
+}
